6b9f3a4506cc72fdf2343dec3b473ac394134f95
Adam <adamdanielgreen@gmail.com>
2025-02-25 18:27:08 +0100

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Adam Daniel Green on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ JournalAppTests/JournalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppTests.swift
+//  JournalAppTests
+//
+//  Created by Adam Daniel Green on 2/25/25.
+//
+
+import Testing
+@testable import JournalApp
+
+struct JournalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
